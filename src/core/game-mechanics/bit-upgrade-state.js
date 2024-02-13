9057bc1ed3ede2fb0dc2eae0991ee204d83a3499
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-02-13 13:22:28 +1300

    Update bit-upgrade-state.js

--- src/core/game-mechanics/bit-upgrade-state.js
+++ src/core/game-mechanics/bit-upgrade-state.js
@@ -27,7 +27,7 @@ export class BitUpgradeState extends GameMechanicState {
     return !this.isUnlocked;
   }
 
-  bitf() {
+  get bitf() {
     return (this.bits % this.id) < (2 ** this.id);
   }
   // eslint-disable-next-line no-empty-function
