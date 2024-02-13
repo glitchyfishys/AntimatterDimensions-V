3d771ce17ca66a32011aa7dc8192816121151e42
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-02-13 13:08:56 +1300

    Update bit-purchasable.js

--- src/core/game-mechanics/bit-purchasable.js
+++ src/core/game-mechanics/bit-purchasable.js
@@ -23,7 +23,7 @@ export class BitPurchasableMechanicState extends PurchasableMechanicState {
     return (this.bits & (1 << this.bitIndex)) !== 0;
   }
 
- bitf(value) {
+ function bitf(value) {
   console.log(this.bits)
   return (this.bits % this.bitIndex) < (2 ** this.bitIndex)
 }
