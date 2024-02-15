edb0ff90e67405d68a136760c2620c54a670bba7
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-02-16 10:57:10 +1300

    Update glyph-effects.js

--- src/core/glyph-effects.js
+++ src/core/glyph-effects.js
@@ -204,7 +204,7 @@ export function makeGlyphEffectBitmask(effectList) {
   return effectList.reduce((mask, eff) => mask + (1 << GlyphEffects[eff].bitmaskIndex), 0);
 }
 
-export function getGlyphEffectsFromBitmask(bitmask, id = "pow") {
+export function getGlyphEffectsFromBitmask(bitmask, id) {
   return orderedEffectList
     .map(effectName => GlyphEffects[effectName])
     .filter(effect => (bitmask & (1 << effect.bitmaskIndex)) !== 0 && effect.id.includes(id) );
