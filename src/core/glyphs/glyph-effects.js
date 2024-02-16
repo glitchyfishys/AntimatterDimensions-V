cd4dc15938844c24e6f8a7bae88d95ccba9f84ea
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-02-16 17:33:51 +1300

    Update glyph-effects.js

--- src/core/glyphs/glyph-effects.js
+++ src/core/glyphs/glyph-effects.js
@@ -111,7 +111,7 @@ export function getGlyphEffectValuesFromBitmask(bitmask, level, baseStrength, ty
 // Pulls out a single effect value from a glyph's bitmask, returning just the value (nothing for missing effects)
 export function getSingleGlyphEffectFromBitmask(effectName, glyph) {
   const glyphEffect = GlyphEffects[effectName];
-  if ((glyph.effects & (1 << glyphEffect.bitmaskIndex)) === 0 && glyphEffect.includes(glyph.type)) {
+  if ((glyph.effects & (1 << glyphEffect.bitmaskIndex)) === 0 && glyphEffect.id.includes(glyph.type)) {
     return undefined;
   }
   return glyphEffect.effect(getAdjustedGlyphLevel(glyph), Pelle.isDoomed ? Pelle.glyphStrength : glyph.strength);
