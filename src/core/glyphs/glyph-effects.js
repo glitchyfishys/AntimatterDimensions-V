e430c356772f00d6d640cfbc17b6876c4c05dc1a
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-02-16 18:12:00 +1300

    Update glyph-effects.js

--- src/core/glyphs/glyph-effects.js
+++ src/core/glyphs/glyph-effects.js
@@ -68,7 +68,7 @@ export function getGlyphEffectValues(effectKey) {
     throw new Error(`Unknown Glyph effect requested "${effectKey}"'`);
   }
   return player.reality.glyphs.active
-    .filter(glyph => (  GlyphEffects[effectKey].id.includes(glyph.type)))
+    .filter(glyph => ( (1 << GlyphEffects[effectKey].bitmaskIndex) & glyph.effects) !== 0 && GlyphEffects[effectKey].id.includes(glyph.type)))
     .filter(glyph => generatedTypes.includes(glyph.type) === GlyphEffects[effectKey].isGenerated)
     .map(glyph => getSingleGlyphEffectFromBitmask(effectKey, glyph));
 }
