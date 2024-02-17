f0e3c182ee79a5352698bd33067d30d2b89328d2
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-02-17 21:01:02 +1300

    Update time-study-tree-layout.js

--- src/components/tabs/time-studies/time-study-tree-layout.js
+++ src/components/tabs/time-studies/time-study-tree-layout.js
@@ -79,15 +79,27 @@ export class TimeStudyTreeLayout {
       normalRow(                      TS(71),  TS(72),  TS(73)                        ),
       normalRow(                      TS(81),  TS(82),  TS(83)                        ),
       normalRow(                      TS(91),  TS(92),  TS(93)                        ),
-      normalRow(                      TS(101), TS(102), TS(103)                       ),
-      normalRow(                       EC(7),  TS(111),  null                         ),
+      normalRow(                      TS(101), TS(102), TS(103)                       )
+    );
+    
+   if(!Perk.studyECRequirement.isBought){
+    this.rows.push(
+      normalRow(                      EC(6),  TS(111),  EC(7)                        ),
+      normalRow(               null, TS(121), TS(122), TS(123), null                 ),
+      normalRow(               null, TS(131), TS(132), TS(133), EC(8)                )
+    );
+   } else{
+      normalRow(                       null,  TS(111),  EC(7)                         ),
       normalRow(                      TS(121), TS(122), TS(123)                       ),
-      normalRow(               EC(6), TS(131), TS(132), TS(133), EC(8)                ),
+      normalRow(               EC(6), TS(131), TS(132), TS(133), EC(8)                )
+
+   }
+    this.rows.push(
       normalRow(                      TS(141), TS(142), TS(143)                       ),
       normalRow(               null,   EC(9), TS(151),   null,   EC(4)                ),
       normalRow(                          TS(161), TS(162)                            )
     );
-
+    
     if (type === STUDY_TREE_LAYOUT_TYPE.ALTERNATIVE_181 || type === STUDY_TREE_LAYOUT_TYPE.ALTERNATIVE_62_181 ||
       type === STUDY_TREE_LAYOUT_TYPE.ALTERNATIVE_TRIAD_STUDIES) {
       this.rows.push(
