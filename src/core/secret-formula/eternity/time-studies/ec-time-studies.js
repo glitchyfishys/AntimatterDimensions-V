abe3f8d493df1bc949544f284ad768935fd0c0bb
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-02-18 16:01:47 +1300

    Update ec-time-studies.js

--- src/core/secret-formula/eternity/time-studies/ec-time-studies.js
+++ src/core/secret-formula/eternity/time-studies/ec-time-studies.js
@@ -64,8 +64,9 @@ export const ecTimeStudies = [
   {
     id: 6,
     cost: 85,
-    requirement: [ 121, () => Perk.bypassEC5Lock.isBought],
+    requirement: [ 121, this.m ],
     reqType: TS_REQUIREMENT_TYPE.AT_LEAST_ONE,
+    m: () => (player.timestudy.studies.includes(111) && TeresaUnlocks.EC6ReqireActive.isUnlocked),
     secondary: {
       resource: "Replicanti Galaxies",
       current: () => player.replicanti.galaxies,
