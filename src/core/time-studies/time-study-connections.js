4032820f7aa12a837d224a2566e1497b6c30b723
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-02-19 15:27:36 +1300

    Update time-study-connections.js

--- src/core/time-studies/time-study-connections.js
+++ src/core/time-studies/time-study-connections.js
@@ -115,13 +115,13 @@ TimeStudy.allConnections = (function() {
 
     [TS(181), EC(10)],
     
-    [TS(181), TS(191), () => !Perk.studyECRequirement.isBought],
-    [TS(181), TS(192), () => !Perk.studyECRequirement.isBought],
-    [TS(181), TS(193), () => !Perk.studyECRequirement.isBought],
+    [TS(181), TS(191), () => !EffarigUnlock.EC10Requirement.canBeApplied],
+    [TS(181), TS(192), () => !EffarigUnlock.EC10Requirement.canBeApplied],
+    [TS(181), TS(193), () => !EffarigUnlock.EC10Requirement.canBeApplied],
     
-    [EC(10), TS(191), () => Perk.studyECRequirement.isBought],
-    [EC(10), TS(192), () => Perk.studyECRequirement.isBought],
-    [EC(10), TS(193), () => Perk.studyECRequirement.isBought],
+    [EC(10), TS(191), () => EffarigUnlock.EC10Requirement.canBeApplied],
+    [EC(10), TS(192), () => EffarigUnlock.EC10Requirement.canBeApplied],
+    [EC(10), TS(193), () => EffarigUnlock.EC10Requirement.canBeApplied],
 
     [TS(192), TS(201)],
 
