43ef2bcf307bb08d90bfb9c5dc63aa70df9fecb1
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-02-22 15:42:56 +1300

    Create index.js

---
+++ src/core/secret-formula/glitch/index.js
@@ -0,0 +1,14 @@
+import {preinfinityUG } ./preinfinity;
+
+export const reality = {
+  automator,
+  cosmeticGlyphs,
+  glyphCosmeticSets,
+  glyphEffects,
+  glyphSacrifice,
+  glyphTypes,
+  imaginaryUpgrades,
+  perks,
+  perkConnections,
+  upgrades: realityUpgrades
+};
