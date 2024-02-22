f17b1210f283a613ec81523deaad7da9896f5878
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-02-22 15:21:56 +1300

    Update preinfinity.js

--- src/core/secret-formula/glitch/preinfinity.js
+++ src/core/secret-formula/glitch/preinfinity.js
@@ -2,13 +2,24 @@
 
 export const preinfinityUG = [
   {
-    name: "Cosmically Duplicate",
+    name: "dimless",
     id: 0,
-    requirement: "Complete your first manual Eternity without using Replicanti Galaxies",
+    requirement: "reach 1e15 antimater without dimboosts",
     checkRequirement: () => player.antimatter.e >= 15 && player.dimensionBoosts === 0,
     canLock: true,
     lockEvent: "gain a Replicanti Galaxy",
-    description: "Replicanti speed is multiplied based on Replicanti Galaxies",
+    description: "AD * (1 + DB /5)",
+    effect: () => 1 + player.dimensionBoosts / 5,
+    formatEffect: value => formatX(value, 2, 2)
+  },
+  {
+  name: "costless",
+    id: 1,
+    requirement: "holder",
+    checkRequirement: () => player.antimatter.e >= 25 && player.dimensionBoosts === 1,
+    canLock: true,
+    lockEvent: "gain a Replicanti Galaxy",
+    description: "holder",
     effect: () => 1 + player.dimensionBoosts / 5,
     formatEffect: value => formatX(value, 2, 2)
   },
