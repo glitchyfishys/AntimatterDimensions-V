4df448b42d6a2c3ef6302ac671e196fad1dc8092
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-02-22 14:27:38 +1300

    Update entropy.vue

--- src/components/tabs/glitch/entropy.vue
+++ src/components/tabs/glitch/entropy.vue
@@ -8,18 +8,8 @@ export default {
 
 <template>
     <div class="c-entropy-infotext">
-      Every completed row of purchased upgrades increases your Glyph level by {{ formatInt(1) }}.
+      hello destroyer.
     </div>
-    <div
-      v-for="row in 0"
-      :key="row"
-      class="l-reality-upgrade-grid__row"
-    >
-      <RealityUpgradeButton
-        v-for="column in 5"
-        :key="id(row, column)"
-        :upgrade="upgrades[id(row, column)]"
-      />
 </template>
 
 <style scoped>
