3c9f7108403cc0f1ed230a9d656341a2393847f3
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-02-22 14:37:38 +1300

    Create pre infinity.vue

---
+++ src/components/tabs/glitch/pre infinity.vue
@@ -0,0 +1,19 @@
+<script>
+
+export default {
+  name: "preinfinity"
+};
+</script>
+
+<template>
+    <div class="c-entropy-infotext">
+      hello destroyer.
+    </div>
+</template>
+
+<style scoped>
+.c-reality-upgrade-infotext {
+  color: var(--color-text);
+  margin: -1rem 0 1.5rem;
+}
+</style>
