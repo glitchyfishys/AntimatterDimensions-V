a75f70d9591d7d879856d984dc3687f44df7682f
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-02-24 11:32:45 +1300

    Update glitchpreinfinityUG.js

--- src/core/glitchpreinfinityUG.js
+++ src/core/glitchpreinfinityUG.js
@@ -1,1 +1,104 @@
+import { BitPurchasableMechanicState, RebuyableMechanicState } from "./game-mechanics";
 
+class preinfinityUG extends BitPurchasableMechanicState {
+  constructor(config) {
+    super(config);
+    this.registerEvents(config.checkEvent, () => this.tryUnlock());
+  }
+
+  get name() {
+    return this.config.name;
+  }
+
+  get shortDescription() {
+    return this.config.shortDescription ? this.config.shortDescription() : "";
+  }
+
+  get requirement() {
+    return typeof this.config.requirement === "function" ? this.config.requirement() : this.config.requirement;
+  }
+
+  get lockEvent() {
+    return typeof this.config.lockEvent === "function" ? this.config.lockEvent() : this.config.lockEvent;
+  }
+
+  get currency() {
+    return Currency.antimatter;
+  }
+
+  get bitIndex() {
+    return this.id;
+  }
+
+  get bits() {
+    return player.reality.upgradeBits;
+  }
+
+  set bits(value) {
+    player.reality.upgradeBits = value;
+  }
+
+  get hasPlayerLock() {
+    return (player.reality.reqLock.reality & (1 << this.bitIndex)) !== 0;
+  }
+
+  set hasPlayerLock(value) {
+    if (value) player.reality.reqLock.reality |= 1 << this.bitIndex;
+    else player.reality.reqLock.reality &= ~(1 << this.bitIndex);
+  }
+
+  get isLockingMechanics() {
+    const shouldBypass = this.config.bypassLock?.() ?? false;
+    return this.hasPlayerLock && this.isPossible && !shouldBypass && !this.isAvailableForPurchase;
+  }
+
+  // Required to be changed this way to avoid direct prop mutation in Vue components
+  setMechanicLock(value) {
+    this.hasPlayerLock = value;
+  }
+
+  toggleMechanicLock() {
+    this.hasPlayerLock = !this.hasPlayerLock;
+  }
+
+  // Note we don't actually show the modal if we already failed or unlocked it
+  tryShowWarningModal(specialLockText) {
+    if (this.isPossible && !this.isAvailableForPurchase) {
+      Modal.upgradeLock.show({ upgrade: this, isImaginary: false, specialLockText });
+    }
+  }
+
+  get isAvailableForPurchase() {
+    return (player.reality.upgReqs & (1 << this.id)) !== 0;
+  }
+
+  get isPossible() {
+    return this.config.hasFailed ? !this.config.hasFailed() : true;
+  }
+
+  tryUnlock() {
+    if (this.isAvailableForPurchase || !this.config.checkRequirement()) return;
+    player.glitch.preinfinity.upgradebits |= (1 << this.id);
+    GameUI.notify.reality(`You've unlocked a Reality Upgrade: ${this.config.name}`);
+    this.hasPlayerLock = false;
+  }
+
+  onPurchased() {
+    const id = this.id;
+    console.log(id)
+  }
+}
+
+preinfinityUG.index = mapGameData(
+  GameDatabase.glitch.preinfinityUG,
+  config => (new preinfinityUG(config))
+);
+
+export const preinfinityUG = id => preinfinityUG.index[id];
+
+export const preinfinityUGs = {
+  all: preinfinityUG.index.compact(),
+  get allBought() {
+    return (player.glitch.preinfinity.upgradeBits >> 1) + 1 === 1 << (GameDatabase.glitch.preinfinityUG.length);
+  }
+};
