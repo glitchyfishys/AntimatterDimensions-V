514e621486a1c229420d3667d51060d1aac41539
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-02-24 17:21:02 +1300

    Update preinfinity.js

--- src/core/secret-formula/glitch/preinfinity.js
+++ src/core/secret-formula/glitch/preinfinity.js
@@ -6,6 +6,7 @@ export const preinfinityUG = [
     requirement: "reach 1e15 antimater without dimboosts",
     hasFailed: () => player.dimensionBoosts != 0 ,
     checkRequirement: () => player.antimatter.e >= 15 && player.dimensionBoosts === 0,
+    checkevent: GAME_EVENT.GAME_TICK_BEFORE,
     description: "AD muliplier from dimboost",
     effect: () => 1 + player.dimensionBoosts / 5,
     formatEffect: value => formatX(value, 2, 2)
@@ -16,6 +17,7 @@ export const preinfinityUG = [
     requirement: " have 1e15 antimatter with one dimboost",
     hasFailed: () => player.dimensionBoosts != 1 ,
     checkRequirement: () => player.antimatter.e >= 25 && player.dimensionBoosts === 1,
+    checkevent: GAME_EVENT.GAME_TICK_BEFORE,
     description: "AD muliplier from dimboost",
     effect: () => 1 + player.dimensionBoosts / 5,
     formatEffect: value => formatX(value, 2, 2)
