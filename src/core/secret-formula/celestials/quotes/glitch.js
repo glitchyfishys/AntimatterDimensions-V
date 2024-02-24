91131db9eb330909f2046a97ae9edb9f6800a6d1
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-02-25 10:31:43 +1300

    Create glitch.js

---
+++ src/core/secret-formula/celestials/quotes/glitch.js
@@ -0,0 +1,26 @@
+export const glitchQuotes = {
+  start: {
+    id: 0,
+    lines: [
+      "Hello Destroyer",
+      "are you the one to help me?",
+      "let's see"
+    ],
+  },
+  dimboost: {
+    id: 1,
+    lines: [
+      "well your going fast",
+      "hmm",
+      "it is you isn't it"
+    ],
+  },
+  dimeight: {
+    id: 2,
+    lines: [
+      "no more than eight",
+      "it looks like your the one"
+    ],
+  }
+  
+};
