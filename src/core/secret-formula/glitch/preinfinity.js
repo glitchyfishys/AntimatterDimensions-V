370428bd9f566b7bf29e699b2c0a5a5652be39c4
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-02-24 15:20:59 +1300

    Update preinfinity.js

--- src/core/secret-formula/glitch/preinfinity.js
+++ src/core/secret-formula/glitch/preinfinity.js
@@ -1,12 +1,10 @@
 
-
 export const preinfinityUG = [
   {
     name: "dimless",
     id: 0,
     requirement: "reach 1e15 antimater without dimboosts",
     checkRequirement: () => player.antimatter.e >= 15 && player.dimensionBoosts === 0,
-    canLock: true,
     lockEvent: "gain a Replicanti Galaxy",
     description: "AD * (1 + DB /5)",
     effect: () => 1 + player.dimensionBoosts / 5,
@@ -15,11 +13,10 @@ export const preinfinityUG = [
   {
   name: "costless",
     id: 1,
-    requirement: "holder",
+    requirement: "place holder",
     checkRequirement: () => player.antimatter.e >= 25 && player.dimensionBoosts === 1,
-    canLock: true,
     lockEvent: "gain a Replicanti Galaxy",
-    description: "holder",
+    description: "place holder",
     effect: () => 1 + player.dimensionBoosts / 5,
     formatEffect: value => formatX(value, 2, 2)
   },
