6acc97e2ce7e339e1a872cdc9b2404eb7c23b74e
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-02-26 11:47:55 +1300

    Create break infinity.js

---
+++ src/core/secret-formula/glitch/break infinity.js
@@ -0,0 +1,48 @@
+
+export const breakinfinityUG = [
+  {
+    name: "limited space",
+    id: 0,
+    requirement: "reach 1e500 antimater without dimboosts",
+    hasFailed: () => player.galaxies > 1 ,
+    checkRequirement: () => player.antimatter.e >= 500,
+    checkevent: GAME_EVENT.GAME_TICK_BEFORE,
+    description: "IP muliplier from galaxys",
+    effect: () => 1 + Math.Pow(player.galaxies, 1.25),
+    formatEffect: value => formatX(value, 2, 2)
+  },
+  {
+  name: "infinitly limiting",
+    id: 1,
+    requirement: "reach 1000 (pending) infinity points with up to four dimboost and one galaxy",
+    hasFailed: () => player.dimensionBoosts > 4 || player.galaxies > 1 ,
+    checkRequirement: () =>  gainedInfinityPoints().greaterThan(1000),
+    checkevent: GAME_EVENT.GAME_TICK_BEFORE,
+    description: "x10 infinity points",
+    effect: () => 10,
+    formatEffect: value => formatX(value, 2, 2)
+  },
+  {
+  name: "chaos",
+    id: 2,
+    requirement: "finish IC1 without infinity dimensions",
+    hasFailed: () => player.dimensions.infinity[0].amount.greaterThan(0),
+    checkRequirement: () => player.antimatter.greaterThan(1.79e308) && player.challenge.infinity.current == 1,
+    checkevent: GAME_EVENT.GAME_TICK_BEFORE,
+    description: "1e50 more infinity points",
+    effect: () => 1e50,
+    formatEffect: value => formatX(value, 2, 2)
+  },
+  {
+    name: "cosmic infinity",
+    id: 3,
+    requirement: "have 1e150 infinity points",
+    hasFailed: () => false,
+    checkRequirement: () => player.infinityPoints.e >= 150,
+    checkevent: GAME_EVENT.GAME_TICK_BEFORE,
+    description: "IP muliplier from galaxies",
+    effect: () => 1 + Math.pow(player.galaxies, 3.5),
+    formatEffect: value => formatX(value, 2, 2)
+  },
+  
+];
