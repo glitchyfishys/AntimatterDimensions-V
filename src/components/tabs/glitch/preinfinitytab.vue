00d41d741a1b91615ed8b79a15b0d342f4df4180
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-02-25 20:10:18 +1300

    Update preinfinitytab.vue

--- src/components/tabs/glitch/preinfinitytab.vue
+++ src/components/tabs/glitch/preinfinitytab.vue
@@ -8,7 +8,7 @@ export default {
   },
   methods: {
     id(row, column) {
-      return (row - 1) * 2 + column - 1;
+      return (row - 1) * 3 + column - 1;
     }
   },
   computed: {
@@ -27,12 +27,12 @@ export default {
     </div>
   
       <div
-      v-for="row in 2"
+      v-for="row in 3"
       :key="row"
       class="l-reality-upgrade-grid__row">
         
       <preinfinityupgradebutton
-        v-for="column in 3"
+        v-for="column in 2"
         :key="id(row, column)"
         :upgrade="upgrades[id(row, column)]"/>
     </div>
