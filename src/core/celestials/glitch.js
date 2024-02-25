1dd34828ac25b5edeee6dd73a5a2f149af5c9cc3
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-02-25 19:40:49 +1300

    Create glitch.js

---
+++ src/core/celestials/glitch.js
@@ -0,0 +1,18 @@
+import { GameDatabase } from "../secret-formula/game-database";
+import { Quotes } from "./quotes";
+
+export const glitch = {
+  displayName: "Glitch",
+  possessiveName: "Glitch's",
+  get isUnlocked() {
+    return true;
+  },
+  initializeRun() {
+    player.celestials.glitch.run = true;
+  },
+  get isRunning() {
+    return player.celestials.glitch.run;
+  },
+  quotes: Quotes.glitch,
+  symbol: "ὡ"
+};
