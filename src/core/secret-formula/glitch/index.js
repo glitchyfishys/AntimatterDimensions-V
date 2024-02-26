97875360355662bd34ad9219d20db1439693d55d
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-02-27 11:58:55 +1300

    Update index.js

--- src/core/secret-formula/glitch/index.js
+++ src/core/secret-formula/glitch/index.js
@@ -1,8 +1,9 @@
 import {preinfinityUG } from "./preinfinity";
 import {breakinfinityUG} from "./break infinity";
+import {eternityUG} from "./eternity";
 
 export const glitch = {
   preinfinityUG: preinfinityUG,
-  breakinfinityUG: breakinfinityUG
-  
+  breakinfinityUG: breakinfinityUG,
+  eternityUG: eternityUG
 };
