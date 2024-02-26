875eff12524c3afac6199765ae252e8fed8bdf22
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-02-26 19:38:33 +1300

    Update preinfinityupgrade.vue

--- src/components/tabs/glitch/preinfinityupgrade.vue
+++ src/components/tabs/glitch/preinfinityupgrade.vue
@@ -88,7 +88,7 @@ export default {
       >
         {{ config.name }}
       </HintText>
-      <span :class="no-pelle-disabled!">
+      <span :class="no-pelle-disabled">
         <DescriptionDisplay :config="config" />
         <template v-if="($viewModel.shiftDown === isAvailableForPurchase) && !isRebuyable">
           <br>
