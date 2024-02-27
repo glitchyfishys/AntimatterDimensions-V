0aeddfe5bc1607d643f9d705ceb7f487d9050a20
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-02-27 15:21:57 +1300

    Update eternity.js

--- src/core/secret-formula/glitch/eternity.js
+++ src/core/secret-formula/glitch/eternity.js
@@ -59,7 +59,7 @@ export const eternityUG = [
     name: "logical tictonics",
     id: 5,
     requirement: "get time study 181 without fully compleing EC1, EC2, E3",
-    hasFailed: () => EternityChallenges.all[0].completions == 5 || EternityChallenges.all[1].completions == 5 || EternityChallenges.all[2].completions == 5 || PlayerProgress.realityUnlocked(),,
+    hasFailed: () => EternityChallenges.all[0].completions == 5 || EternityChallenges.all[1].completions == 5 || EternityChallenges.all[2].completions == 5 || PlayerProgress.realityUnlocked(),
     checkRequirement: () => player.timestudy.studies.includes(181) && !EternityChallenges.all[0].completions == 5 && !EternityChallenges.all[1].completions == 5 && !EternityChallenges.all[2].completions == 5 || !PlayerProgress.realityUnlocked(),
     checkevent: GAME_EVENT.GAME_TICK_BEFORE,
     description: "5000 free tick speed upgrades",
