97b7ddd288333e32c1f5f837f98ffcb3921f5a24
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-02-27 18:47:08 +1300

    Update preinfinity.js

--- src/core/secret-formula/glitch/preinfinity.js
+++ src/core/secret-formula/glitch/preinfinity.js
@@ -1,47 +1,47 @@
 
 export const preinfinityUG = [
   {
-    name: "dimless1",
+    name: "dimless",
     id: 0,
-    requirement: "reach 1e15 antimater without dimboosts",
+    requirement: "reach 1e15 antimater without any dimboosts",
     hasFailed: () => player.dimensionBoosts != 0 || PlayerProgress.infinityUnlocked(),
     checkRequirement: () => player.antimatter.e >= 15 && player.dimensionBoosts == 0 && !PlayerProgress.infinityUnlocked(),
     checkevent: GAME_EVENT.GAME_TICK_BEFORE,
     description: "AD muliplier from dimboosts (pelle effective)",
-    effect: () => 1 + player.dimensionBoosts / 5,
+    effect: () => 1 + player.dimensionBoosts / 4,
     formatEffect: value => formatX(value, 2, 2)
   },
   {
-  name: "dimless2",
+  name: "dimatonal limits",
     id: 1,
-    requirement: "have 1e25 antimatter with one dimboost",
+    requirement: "have 1e25 antimatter with only one dimboost",
     hasFailed: () => player.dimensionBoosts != 1 || PlayerProgress.infinityUnlocked(),
     checkRequirement: () => player.antimatter.e >= 25 && player.dimensionBoosts == 1 && !PlayerProgress.infinityUnlocked(),
     checkevent: GAME_EVENT.GAME_TICK_BEFORE,
     description: "AD muliplier from dimboosts (pelle effective)",
-    effect: () => 1 + player.dimensionBoosts / 5,
+    effect: () => 1 + player.dimensionBoosts / 4,
     formatEffect: value => formatX(value, 2, 2)
   },
   {
-  name: "dimless3",
+  name: "vector 2",
     id: 2,
-    requirement: "have 1e35 antimatter with two dimboosts",
+    requirement: "have 1e35 antimatter with only two dimboosts",
     hasFailed: () => player.dimensionBoosts != 2 || PlayerProgress.infinityUnlocked(),
     checkRequirement: () => player.antimatter.e >= 35 && player.dimensionBoosts == 2 && !PlayerProgress.infinityUnlocked(),
     checkevent: GAME_EVENT.GAME_TICK_BEFORE,
     description: "AD muliplier from dimboosts (pelle effective)",
-    effect: () => 1 + player.dimensionBoosts / 5,
+    effect: () => 1 + player.dimensionBoosts / 4,
     formatEffect: value => formatX(value, 2, 2)
   },
   {
-    name: "dimless4",
+    name: "8th dimensional",
     id: 3,
     requirement: "have 1e150 antimatter with four dimboosts",
     hasFailed: () => player.galaxys == 1 || player.dimensionBoosts > 4 || PlayerProgress.infinityUnlocked(),
     checkRequirement: () => player.antimatter.e >= 150 && player.dimensionBoosts == 4 && !PlayerProgress.infinityUnlocked(),
     checkevent: GAME_EVENT.GAME_TICK_BEFORE,
     description: "AD muliplier from dimboosts (pelle effective)",
-    effect: () => 1 + player.dimensionBoosts / 5,
+    effect: () => 1 + player.dimensionBoosts / 4,
     formatEffect: value => formatX(value, 2, 2)
   },
   {
@@ -69,7 +69,7 @@ export const preinfinityUG = [
   {
     name: "galatic capacity",
     id: 6,
-    requirement: "reach 1.8e308 antimatter one galaxy",
+    requirement: "reach 1.8e308 antimatter with only one antimatter galaxy",
     hasFailed: () => player.galaxies > 1 || PlayerProgress.infinityUnlocked(),
     checkRequirement: () => player.antimatter.greaterThan(1.79e308) && !player.galaxies > 1 && !PlayerProgress.infinityUnlocked(),
     checkevent: GAME_EVENT.GAME_TICK_BEFORE,
