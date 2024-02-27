608ba390ccda1c4f4ed6cc294d2b1a70a01cc663
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-02-27 13:22:15 +1300

    Create eternitytab.vue

---
+++ src/components/tabs/glitch/eternitytab.vue
@@ -0,0 +1,48 @@
+<script>
+import preinfinityupgradebutton from "./preinfinityupgrade";
+  
+export default {
+  name: "glitcheternity",
+  components: {
+    preinfinityupgradebutton
+  },
+  methods: {
+    id(row, column) {
+      return (row - 1) * 3 + column - 1;
+    }
+  },
+  computed: {
+    upgrades: () => eternityUGs.all
+  }
+};
+  
+</script>
+
+<template>
+  <div>
+    <div class="c-eternity-infotext">
+      this is the biggest threshold
+      <br>
+      get a many as you can
+    </div>
+  
+      <div
+      v-for="row in 2"
+      :key="row"
+      class="l-reality-upgrade-grid__row">
+        
+      <preinfinityupgradebutton
+        v-for="column in 3"
+        :key="id(row, column)"
+        :upgrade="upgrades[id(row, column)]"/>
+    </div>
+    
+  </div>
+</template>
+
+<style scoped>
+.c-preinfinity-infotext {
+  color: var(--color-text);
+  margin: 1rem 0 1.5rem;
+}
+</style>
