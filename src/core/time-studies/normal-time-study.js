217fa14c2bf7c29d244df7d894603a6929a3e67d
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-02-28 20:55:20 +1300

    Update normal-time-study.js

--- src/core/time-studies/normal-time-study.js
+++ src/core/time-studies/normal-time-study.js
@@ -7,8 +7,8 @@ NormalTimeStudies.pathList = [
   { path: TIME_STUDY_PATH.INFINITY_DIM, studies: [72, 82, 92, 102], name: "Infinity Dims" },
   { path: TIME_STUDY_PATH.TIME_DIM, studies: [73, 83, 93, 103], name: "Time Dims" },
   { path: TIME_STUDY_PATH.ACTIVE, studies: [121, 131, 141], name: "Active" },
-  { path: TIME_STUDY_PATH.PASSIVE, studies: [122, 132, 142], name: "Passive" },
-  { path: TIME_STUDY_PATH.IDLE, studies: [123, 133, 143], name: "Hybrid" },
+  { path: TIME_STUDY_PATH.PASSIVE, studies: [122, 132, 142], name: "Hybrid" },
+  { path: TIME_STUDY_PATH.IDLE, studies: [123, 133, 143], name: "Idle" },
   { path: TIME_STUDY_PATH.LIGHT, studies: [221, 223, 225, 227, 231, 233], name: "Light" },
   { path: TIME_STUDY_PATH.DARK, studies: [222, 224, 226, 228, 232, 234], name: "Dark" }
 ];
