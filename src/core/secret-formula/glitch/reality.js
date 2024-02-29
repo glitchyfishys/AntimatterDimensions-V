6075792a03a1237c5e4d293871ee41e3652cd658
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-02-29 19:59:35 +1300

    Update reality.js

--- src/core/secret-formula/glitch/reality.js
+++ src/core/secret-formula/glitch/reality.js
@@ -18,9 +18,31 @@ export const realityUG = [
     hasFailed: () => player.realities != 0,
     checkRequirement: () => gainedEternityPoints().greaterThanOrEqualTo("1e1000") && player.dilation.active && player.realities == 0,
     checkevent: GAME_EVENT.GAME_TICK_BEFORE,
-    description: "gain 10 times more tachyon particles",
-    effect: () => 10,
-    formatEffect: value => formatInt(value)
+    description: "gain 25 times more tachyon particles",
+    effect: () => 25,
+    formatEffect: value => formatX(value, 2, 2)
+  },
+  {
+    name: "real time complex",
+    id: 2,
+    requirement: "reach 1e8000 eternity points before your first reality",
+    hasFailed: () => player.realities != 0,
+    checkRequirement: () => player.eternityPoints.e >= 8000 && player.realities == 0,
+    checkevent: GAME_EVENT.GAME_TICK_BEFORE,
+    description: "gain 25 times more perks and realitys ",
+    effect: () => 25,
+    formatEffect: value => formatX(value, 2, 2)
+  },
+  {
+    name: "immisinity",
+    id: 3,
+    requirement: "reach 1e6 RM",
+    hasFailed: () =>false,
+    checkRequirement: () => player.reality.realityMachines.e >= 6,
+    checkevent: GAME_EVENT.GAME_TICK_BEFORE,
+    description: "all TD gain a 1e100 multiplier",
+    effect: () => Decimal.add(1e100),
+    formatEffect: value => formatX(value, 2, 2)
   },
 
 ];
