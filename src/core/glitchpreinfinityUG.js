a11d8f64474c39569e0ee5d12f6485367b94ae7c
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-03-04 14:52:45 +1300

    Update glitchpreinfinityUG.js

--- src/core/glitchpreinfinityUG.js
+++ src/core/glitchpreinfinityUG.js
@@ -47,7 +47,7 @@ class preinfinityUGState extends BitPurchasableMechanicState {
   }
 
   get isUseless() {
-    return this.config.isUsless();
+    return this.config.isUseless();
   }
   
   tryUnlock() {
