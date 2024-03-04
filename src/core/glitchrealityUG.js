756b2e559414a3b4e383843c17d96f527654abf6
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-03-04 17:37:30 +1300

    Update glitchrealityUG.js

--- src/core/glitchrealityUG.js
+++ src/core/glitchrealityUG.js
@@ -47,7 +47,7 @@ class realityUGState extends BitPurchasableMechanicState {
   }
 
   get isUseless() {
-    if(typeof this.config.isUseless === "function") this.config.isUseless()
+    if(typeof this.config.isUseless != "undefined") return (this.config.isUseless() && Pelle.isDoomed)
     return false;
   }
   
