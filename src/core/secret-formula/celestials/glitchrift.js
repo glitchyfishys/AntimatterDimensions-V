c2b0ef21231f9297e5c04561ddaf25ecb732d4a5
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-04-01 16:25:14 +1300

    Create glitchrift.js

---
+++ src/core/secret-formula/celestials/glitchrift.js
@@ -0,0 +1,63 @@
+
+export const GlitchRifts = {
+  alpha: {
+    id: 1,
+    key: "alpha",
+    name: "pre-infinity",
+    drainResource: "IP",
+    percentage: totalFill => player.glitch.preinfinity.upgradebits & 255,
+    percentageToFill: 6,
+    milestones: [
+      {
+        resource: "alpha",
+        requirement: 0.1,
+        description: "fish"
+      },
+    ],
+  },
+  beta: {
+    id: 2,
+    key: "beta",
+    name: "post-infinity",
+    drainResource: "IP",
+    percentage: totalFill => player.glitch.breakinfinity.upgradebits & 255,
+    percentageToFill: 6,
+    milestones: [
+      {
+        resource: "beta",
+        requirement: 0.1,
+        description: "fish"
+      },
+    ],
+  },
+  detla: {
+    id: 3,
+    key: "delta",
+    name: "eternity",
+    drainResource: "IP",
+    percentage: totalFill => player.glitch.eternity.upgradebits & 255,
+    percentageToFill: 6,
+    milestones: [
+      {
+        resource: "delta",
+        requirement: 0.1,
+        description: "fish"
+      },
+    ],
+  },
+  gamma: {
+    id: 4,
+    key: "gamma",
+    name: "reality",
+    drainResource: "IP",
+    percentage: totalFill => player.glitch.reality.upgradebits & 255,
+    percentageToFill: 6,
+    milestones: [
+      {
+        resource: "gamma",
+        requirement: 0.1,
+        description: "fish"
+      },
+    ],
+  },
+};
