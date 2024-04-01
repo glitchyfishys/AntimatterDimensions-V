00412d7271d0ae068c7d407e41ed11b3425ff2f8
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-04-01 17:38:35 +1300

    Update entropy.vue

--- src/components/tabs/glitch/entropy.vue
+++ src/components/tabs/glitch/entropy.vue
@@ -20,7 +20,7 @@ export default {
     </div>
 
   <GlitchRift
-          v-for="rift in strikes" :key="rift.config.id" :rift="rift"
+          v-for="rift in strikes" :key="rift.config.id" :rift="GlitchRift"
         />
   
 </template>
