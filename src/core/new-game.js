02c4309adb7a53ac80495d26afd829b2f3c4b36a
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-05-15 09:57:40 +1200

    Update new-game.js

--- src/core/new-game.js
+++ src/core/new-game.js
@@ -63,6 +63,9 @@ export const NG = {
     const glyphCosmetics = JSON.stringify(player.reality.glyphs.cosmetics);
     const speedrunRecords = JSON.stringify(player.speedrun.previousRuns);
     const hasSpeedrun = player.speedrun.isUnlocked;
+    const presets = JSON.stringify(player.timestudy.presets);
+    const companions = JSON.stringify(Glyphs.allGlyphs.filter(g => g.type === "companion"));
+    
     Modal.hideAll();
     Quote.clearAll();
     GameStorage.hardReset();
@@ -82,10 +85,16 @@ export const NG = {
     player.reality.glyphs.cosmetics = JSON.parse(glyphCosmetics);
     player.speedrun.previousRuns = JSON.parse(speedrunRecords);
     player.speedrun.isUnlocked = hasSpeedrun;
+    player.timestudy.presets = JSON.parse(presets);
+    JSON.parse(companions).forEach(g => {
+      Glyphs.addToInventory(g);
+    });
     Themes.find(Theme.currentName()).set();
     Notations.all.find(n => n.name === player.options.notation).setAsCurrent();
     ADNotations.Settings.exponentCommas.min = 10 ** player.options.notationDigits.comma;
     ADNotations.Settings.exponentCommas.max = 10 ** player.options.notationDigits.notation;
     player.lastUpdate = Date.now();
+
+    
   }
 };
