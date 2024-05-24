d0e9c118c63fbd851c8595b6adb44d7eb06db35c
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-05-24 20:19:48 +1200

    Update dark-matter-dimension.js

--- src/core/celestials/laitela/dark-matter-dimension.js
+++ src/core/celestials/laitela/dark-matter-dimension.js
@@ -111,7 +111,7 @@ export class DarkMatterDimensionState extends DimensionState {
   get adjustedStartingCost() {
     const tiers = [null, 0, 2, 5, 13, 25];
     return 10 * Decimal.pow(COST_MULT_PER_TIER, tiers[this.tier]).times(
-      SingularityMilestone.darkDimensionCostReduction.effectOrDefault(1));
+      SingularityMilestone.darkDimensionCostReduction.effectOrDefault(1)).toNumber();
   }
 
   get rawIntervalCost() {
