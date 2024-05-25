95ecc4511cf41340c2ff3772417692e250071208
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-05-25 17:42:25 +1200

    Update realitytab.vue

--- src/components/tabs/glitch/realitytab.vue
+++ src/components/tabs/glitch/realitytab.vue
@@ -29,12 +29,12 @@ export default {
     </div>
   
       <div
-      v-for="row in 3"
+      v-for="row in 7"
       :key="row"
       class="l-reality-upgrade-grid__row">
         
       <preinfinityupgradebutton
-        v-for="column in 4"
+        v-for="column in 2"
         :key="id(row, column)"
         :upgrade="upgrades[id(row, column)]"/>
     </div>
