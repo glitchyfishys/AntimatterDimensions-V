1f60236ab23937646111e37c627927d52846ce5b
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-05-27 10:25:21 +1200

    Update glitchrift.js
    oops

--- src/core/celestials/glitch/glitchrift.js
+++ src/core/celestials/glitch/glitchrift.js
@@ -94,7 +94,7 @@ class GRift extends GameMechanicState {
 
   get effects() {
     let effects = [];
-    this.milestones.forEach(milestone => effects.push( milestime.format(milestone.effectOrDefault(milestone.base))));
+    this.milestones.forEach(milestone => effects.push( milestone.format(milestone.effectOrDefault(milestone.base))));
     return effects;
   }
 
