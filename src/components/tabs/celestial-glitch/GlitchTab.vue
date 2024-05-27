9ebefe0bd8da773a1c6762c5d2465632c43d9fb7
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-05-27 15:13:40 +1200

    Update GlitchTab.vue

--- src/components/tabs/celestial-glitch/GlitchTab.vue
+++ src/components/tabs/celestial-glitch/GlitchTab.vue
@@ -6,7 +6,7 @@ import PrimaryButton from "@/components/PrimaryButton";
 import PrimaryToggleButton from "@/components/PrimaryToggleButton";
 
 export default {
-  name: "EnslavedTab",
+  name: "GlitchTab",
   components: {
     CelestialQuoteHistory,
     PrimaryButton,
@@ -14,182 +14,27 @@ export default {
     BlackHoleChargingSliders
   },
   data: () => ({
-    isStoringBlackHole: false,
-    isStoringReal: false,
-    autoStoreReal: false,
-    offlineEnabled: false,
-    hasAutoRelease: false,
     isRunning: false,
-    completed: false,
-    storedBlackHole: 0,
-    storedReal: 0,
-    storedRealEffiency: 0,
-    storedRealCap: 0,
-    autoRelease: false,
-    autoReleaseSpeed: 0,
-    unlocks: [],
-    buyableUnlocks: [],
     quote: "",
-    currentSpeedUp: 0,
-    hintsUnlocked: false,
-    canModifyGameTimeStorage: false,
-    canChangeStoreTime: false,
-    canChangeStoreRealTime: false,
-    canDischarge: false,
-    canAutoRelease: false,
-    hasNoCharge: true,
-    hasReachedCurrentCap: false,
   }),
   computed: {
-    storedRealEfficiencyDesc() {
-      return formatPercents(this.storedRealEffiency);
-    },
-    storedRealCapDesc() {
-      return timeDisplayShort(this.storedRealCap);
-    },
-    unlocksInfo() {
-      return ENSLAVED_UNLOCKS;
-    },
-    nerfedBlackHoleTime() {
-      return Enslaved.storedTimeInsideEnslaved(this.storedBlackHole);
-    },
-    realityTitle() {
-      if (this.isRunning) return "You are inside The Nameless Ones' Reality";
-      return "Start The Nameless Ones' Reality";
-    },
-    runButtonClassObject() {
-      return {
-        "c-enslaved-run-button__icon": true,
-        "c-enslaved-run-button__icon--running": this.isRunning,
-        "c-celestial-run-button--clickable": !this.isDoomed,
-        "o-pelle-disabled-pointer": this.isDoomed
-      };
-    },
-    runDescription() {
-      return GameDatabase.celestials.descriptions[2].effects().split("\n");
-    },
-    realTimeButtonText() {
-      if (!this.offlineEnabled) return "Offline Progress is disabled";
-      if (this.autoStoreReal) return "Offline time stored";
-      return "Offline time used for production";
-    },
-    // Use this here since Nameless has a fairly non-standard character, and SFCs don't support using \uf0c1
-    enslavedSymbol: () => Enslaved.symbol,
-    isDoomed: () => Pelle.isDoomed,
-    storeGameTimeClass() {
-      return {
-        "o-enslaved-mechanic-button": true,
-        "o-enslaved-mechanic-button--clickable": this.canModifyGameTimeStorage,
-        "o-enslaved-mechanic-button--storing-time": this.isStoringBlackHole,
-        "l-fixed-setting": !this.canModifyGameTimeStorage,
-        "o-pelle-disabled": this.isDoomed
-      };
-    },
-    storeRealTimeClass() {
-      return {
-        "o-enslaved-mechanic-button": true,
-        "o-enslaved-mechanic-button--clickable": !this.isDoomed,
-        "o-enslaved-mechanic-button--storing-time": this.isStoringReal,
-        "l-fixed-setting": !this.canChangeStoreRealTime,
-        "o-pelle-disabled": this.isDoomed
-      };
-    },
-    dischargeClass() {
-      return {
-        "o-enslaved-mechanic-button": true,
-        "o-enslaved-mechanic-button--clickable": !this.isDoomed,
-        "l-fixed-setting": !this.canDischarge || this.hasNoCharge,
-        "o-pelle-disabled": this.isDoomed
-      };
-    },
-    doomedDisabledClass() {
-      return { "o-pelle-disabled": this.isDoomed };
-    },
-    mechanicButtonClass() {
-      return {
-        "o-enslaved-mechanic-button": true,
-        "o-enslaved-mechanic-button--clickable": !this.isDoomed
-      };
-    }
+ 
   },
   watch: {
-    autoRelease(newValue) {
-      player.celestials.enslaved.isAutoReleasing = newValue;
-    }
   },
   methods: {
     update() {
-      this.isStoringBlackHole = Enslaved.isStoringGameTime;
-      this.storedBlackHole = player.celestials.enslaved.stored;
-      this.isStoringReal = Enslaved.isStoringRealTime;
-      this.autoStoreReal = player.celestials.enslaved.autoStoreReal;
-      this.offlineEnabled = player.options.offlineProgress;
-      this.hasAutoRelease = Ra.unlocks.autoPulseTime.canBeApplied;
       this.isRunning = Enslaved.isRunning;
-      this.completed = Enslaved.isCompleted && !this.isDoomed;
-      this.storedReal = player.celestials.enslaved.storedReal;
-      this.storedRealEffiency = Enslaved.storedRealTimeEfficiency;
-      this.storedRealCap = Enslaved.storedRealTimeCap;
-      this.unlocks = Array.from(player.celestials.enslaved.unlocks);
-      this.buyableUnlocks = Object.values(ENSLAVED_UNLOCKS).map(x => Enslaved.canBuy(x));
       this.quote = Enslaved.quote;
-      this.autoRelease = player.celestials.enslaved.isAutoReleasing;
-      this.autoReleaseSpeed = Enslaved.isAutoReleasing ? Enslaved.autoReleaseSpeed : 0;
-      this.currentSpeedUp = Enslaved.currentBlackHoleStoreAmountPerMs;
-      this.hintsUnlocked = EnslavedProgress.hintsUnlocked.hasProgress;
-      this.canModifyGameTimeStorage = Enslaved.canModifyGameTimeStorage;
-      this.canChangeStoreTime = Enslaved.canModifyGameTimeStorage;
-      this.canChangeStoreRealTime = Enslaved.canModifyRealTimeStorage;
-      this.canDischarge = Enslaved.canRelease(false);
-      this.canAutoRelease = Enslaved.canRelease(true);
-      this.hasNoCharge = player.celestials.enslaved.stored === 0;
-      this.hasReachedCurrentCap = this.storedReal === this.storedRealCap;
-    },
-    toggleStoreBlackHole() {
-      Enslaved.toggleStoreBlackHole();
-    },
-    toggleStoreReal() {
-      Enslaved.toggleStoreReal();
-    },
-    toggleAutoStoreReal() {
-      if (!this.offlineEnabled) return;
-      Enslaved.toggleAutoStoreReal();
-    },
-    useStored() {
-      Enslaved.useStoredTime(false);
-    },
-    timeDisplayShort(ms) {
-      return timeDisplayShort(ms);
-    },
-    timeUntilBuy(price) {
-      return Math.max((price - this.storedBlackHole) / this.currentSpeedUp, 0);
-    },
-    buyUnlock(info) {
-      Enslaved.buyUnlock(info);
     },
     startRun() {
       if (this.isDoomed) return;
       Modal.celestials.show({ name: "The Nameless Ones'", number: 2 });
     },
-    hasUnlock(info) {
-      return Enslaved.has(info);
-    },
-    canBuyUnlock(info) {
-      // This (rather than just using Enslaved.canBuy(info) and removing this.buyableUnlocks)
-      // is needed for proper reactivity of button styles (e.g., if you get a level 5000 glyph
-      // while on the Nameless tab).
-      return this.buyableUnlocks[info.id];
-    },
-    unlockClassObject(info) {
-      return {
-        "o-enslaved-shop-button--bought": this.hasUnlock(info),
-        "o-enslaved-shop-button--available": this.canBuyUnlock(info)
-      };
-    },
     glitchStyle(x) {
-      const xScale = 15 / 27;
-      const yScale = 5;
-      const dx = (x - 13) * xScale + (Math.random() * 2 - 1) * 0.85;
+      const xScale = 15 / 19;
+      const yScale = 6;
+      const dx = (x - 13) * xScale + (Math.random() * 2 - 1) * 0.9;
       const dy = (Math.random() * 2 - 1) * yScale;
       const height = (Math.pow(Math.random(), 1.5) + 0.25) * 3 * yScale;
       return {
@@ -204,6 +49,7 @@ export default {
 <template>
   <div class="l-glitch-tab">
     blank for now
+    <BlackHoleChargingSliders>
   </div>
 </template>
 
