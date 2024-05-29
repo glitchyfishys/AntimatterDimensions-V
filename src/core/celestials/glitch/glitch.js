86f621fb5dfbca941b50d9e3bf2040f6a4b40389
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-05-29 13:47:00 +1200

    Update glitch.js

--- src/core/celestials/glitch/glitch.js
+++ src/core/celestials/glitch/glitch.js
@@ -8,8 +8,23 @@ export const glitch = {
   get isUnlocked() {
     return GlitchRifts.gamma.milestones[5].effectOrDefault(0) == 0 ? false : true;
   },
-  initializeRun() {
-    player.celestials.glitch.run = true;
+  initializeRun(layer = 0) {
+    if(layer == 0){
+      player.celestials.glitch.run = true;
+      player.celestials.teresa.run = true;
+      player.celestials.effarig.run = true;
+      player.celestials.enslaved.run = true;
+      player.celestials.v.run = true;
+      player.celestials.ra.run = true;
+      player.celestials.laitela.run = true;
+    }
+
+    
+  },
+  leaveRun() {
+    player.celestials.glitch.run = false;
+
+    
   },
   get isRunning() {
     return player.celestials.glitch.run;
