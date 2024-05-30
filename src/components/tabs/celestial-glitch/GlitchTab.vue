97dc47e3a216b66ba69d9c04e32c618f77d96e24
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-05-30 13:47:33 +1200

    Update GlitchTab.vue

--- src/components/tabs/celestial-glitch/GlitchTab.vue
+++ src/components/tabs/celestial-glitch/GlitchTab.vue
@@ -29,6 +29,10 @@ export default {
     startRun() {
       if (this.isDoomed) return;
       Modal.celestials.show({ name: "glitch'", number: 6 });
+    },
+    effects(id){
+      if ((player.celestials.glitch.augment.effectbits & (1 << id)) > 0) player.celestials.glitch.augment.effectbits |= (1 << id);
+      else player.celestials.glitch.augment.effectbits &= ~(1 << id)
     }
   },
 };
@@ -41,6 +45,13 @@ export default {
   <CelestialQuoteHistory celestial="glitch"/>
     
     <GlitchRunButton />
+
+    <PrimaryToggleButton
+      v-model="effects(0)"
+      on="remove Teresa's Reality"
+      off="enable Teresa's Reality"
+      class="o-primary-btn--subtab-option"
+    />
     
     <BlackHoleChargingSliders />
     
