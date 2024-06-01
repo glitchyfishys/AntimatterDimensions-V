8fea394db42349bf900db3b750a466e909759df1
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-06-01 15:15:48 +1200

    Update glyph-core.js

--- src/core/glyphs/glyph-core.js
+++ src/core/glyphs/glyph-core.js
@@ -853,8 +853,8 @@ export function getAdjustedGlyphLevel(glyph, realityGlyphBoost = Glyphs.levelBoo
   const level = glyph.level;
   if (!ignoreCelestialEffects) {
     if (Pelle.isDoomed) return Math.min(level, Pelle.glyphMaxLevel);
-    if (Enslaved.isRunning) return Math.max(level, Enslaved.glyphLevelMin);
     if (Effarig.isRunning) return Math.min(level, Effarig.glyphLevelCap);
+    if (Enslaved.isRunning) return Math.max(level, Enslaved.glyphLevelMin);
   }
   if (BASIC_GLYPH_TYPES.includes(glyph.type)) return level + realityGlyphBoost;
   return level;
