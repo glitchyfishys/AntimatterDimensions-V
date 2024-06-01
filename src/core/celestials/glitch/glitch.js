8778b0913e5f22e0f5e25506ad4820ffefe605d4
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-06-01 21:14:14 +1200

    Update glitch.js

--- src/core/celestials/glitch/glitch.js
+++ src/core/celestials/glitch/glitch.js
@@ -54,7 +54,7 @@ export const Glitch = {
       //could be bad
       disChargeAll();
       Glyphs.unequipAll(true);
-      for(i=0;i<5;i++) if((Glyphs.inventory.filter(x => x == null ? false :(x.type == "cursed")).length + Glyphs.active.filter(x => x == null ? false :(x.type == "cursed")).length) < 6) Glyphs.addToInventory(GlyphGenerator.cursedGlyph());
+      for(i=0;i<5;i++) if((Glyphs.inventory.filter(x => x == null ? false :(x.type == "cursed")).length + Glyphs.active.filter(x => x == null ? false :(x.type == "cursed")).length) < 5) Glyphs.addToInventory(GlyphGenerator.cursedGlyph());
       for(i=0;i<5;i++) Glyphs.equip(player.reality.glyphs.inventory.filter(x=> x.type == "cursed")[0],i);
 
       
