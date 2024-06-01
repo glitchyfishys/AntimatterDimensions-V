f855a59d887c4a58e1d2d3615778cdece0bd6cab
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-06-01 14:08:17 +1200

    Update preinfinity.js

--- src/core/secret-formula/glitch/preinfinity.js
+++ src/core/secret-formula/glitch/preinfinity.js
@@ -3,7 +3,7 @@ export const preinfinityUG = [
   {
     name: "dimless",
     id: 0,
-    requirement: "reach 1e15 antimater without any dimboosts, no galaxies",
+    requirement: `reach ${format(1e15)} antimater without any dimboosts, no galaxies`,
     hasFailed: () => player.dimensionBoosts != 0 || player.galaxys > 0 || PlayerProgress.infinityUnlocked(),
     checkRequirement: () => player.antimatter.e >= 15 && player.dimensionBoosts == 0 && !PlayerProgress.infinityUnlocked(),
     checkevent: GAME_EVENT.GAME_TICK_BEFORE,
@@ -14,7 +14,7 @@ export const preinfinityUG = [
   {
   name: "dimatonal limits",
     id: 1,
-    requirement: "have 1e25 antimatter with only one dimboost, no galaxies",
+    requirement: `have ${format(1e25)} antimatter with only one dimboost, no galaxies`,
     hasFailed: () => player.dimensionBoosts >1 || player.galaxys > 0 || PlayerProgress.infinityUnlocked(),
     checkRequirement: () => player.antimatter.e >= 25 && player.dimensionBoosts <= 1 && !PlayerProgress.infinityUnlocked(),
     checkevent: GAME_EVENT.GAME_TICK_BEFORE,
@@ -25,7 +25,7 @@ export const preinfinityUG = [
   {
   name: "vector 2",
     id: 2,
-    requirement: "have 1e35 antimatter with upto two dimboosts, no galaxies",
+    requirement: `have ${format(1e35)} antimatter with upto two dimboosts, no galaxies`,
     hasFailed: () => player.dimensionBoosts > 2 || player.galaxys > 0 || PlayerProgress.infinityUnlocked(),
     checkRequirement: () => player.antimatter.e >= 35 && player.dimensionBoosts <= 2 && !PlayerProgress.infinityUnlocked(),
     checkevent: GAME_EVENT.GAME_TICK_BEFORE,
@@ -36,7 +36,7 @@ export const preinfinityUG = [
   {
     name: "8th dimensional",
     id: 3,
-    requirement: "have 1e150 antimatter with upto six dimboosts, no galaxies",
+    requirement: `have ${format(1e150)} antimatter with upto six dimboosts, no galaxies`,
     hasFailed: () => player.galaxys == 1 || player.dimensionBoosts > 6 || player.galaxys > 0 || PlayerProgress.infinityUnlocked(),
     checkRequirement: () => player.antimatter.e >= 150 && player.dimensionBoosts <= 6 && !PlayerProgress.infinityUnlocked(),
     checkevent: GAME_EVENT.GAME_TICK_BEFORE,
@@ -47,7 +47,7 @@ export const preinfinityUG = [
   {
     name: "galatic inforcement",
     id: 4,
-    requirement: "have 1e7 antimatter with no dimboosts, one galaxy, up to ten 2nd dimentsions",
+    requirement: `have ${format(1e7)} antimatter with no dimboosts, one galaxy, up to ten 2nd dimentsions`,
     hasFailed: () => player.dimensions.antimatter[1].amount.greaterThan(10) || player.galaxys > 1 || player.dimensionBoosts > 0 || PlayerProgress.infinityUnlocked(),
     checkRequirement: () => player.antimatter.e >= 7 && !player.dimensions.antimatter[1].amount.greaterThan(20) && player.galaxies <= 1 && !player.dimensionBoosts > 0 && !PlayerProgress.infinityUnlocked(),
     checkevent: GAME_EVENT.GAME_TICK_BEFORE,
@@ -58,7 +58,7 @@ export const preinfinityUG = [
   {
     name: "galatic limitaion",
     id: 5,
-    requirement: "have 1e25 antimatter with no dimboosts, one galaxy, up to 20 4st dimentsions",
+    requirement: `have ${format(1e25)} antimatter with no dimboosts, one galaxy, up to 20 4st dimentsions`,
     hasFailed: () => player.dimensions.antimatter[3].amount.greaterThan(20) || player.galaxys > 1 || player.dimensionBoosts > 0 || PlayerProgress.infinityUnlocked(),
     checkRequirement: () => player.antimatter.e >= 25 && !player.dimensions.antimatter[3].amount.greaterThan(20) && player.galaxies <= 1 && !player.dimensionBoosts > 0 && !PlayerProgress.infinityUnlocked(),
     checkevent: GAME_EVENT.GAME_TICK_BEFORE,
@@ -69,7 +69,7 @@ export const preinfinityUG = [
   {
     name: "galatic capacity",
     id: 6,
-    requirement: "reach 1.8e308 antimatter with only one antimatter galaxy",
+    requirement: `reach ${format(1.8e308)} antimatter with only one antimatter galaxy`,
     hasFailed: () => player.galaxies > 1 || PlayerProgress.infinityUnlocked(),
     checkRequirement: () => player.antimatter.greaterThan(1.79e308) && player.galaxies <= 1 && !PlayerProgress.infinityUnlocked(),
     checkevent: GAME_EVENT.GAME_TICK_BEFORE,
@@ -80,7 +80,7 @@ export const preinfinityUG = [
   {
     name: "galatic instance",
     id: 7,
-    requirement: "have 3 antimatter galaxys without infinity broken",
+    requirement: `have ${format(3)} antimatter galaxys without infinity broken`,
     hasFailed: () => PlayerProgress.hasBroken() || player.challenge.normal.current != 10,
     checkRequirement: () => player.galaxies >= 3 && !PlayerProgress.hasBroken(),
     checkevent: GAME_EVENT.GAME_TICK_BEFORE,
