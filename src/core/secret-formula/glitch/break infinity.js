780e71fba4bc5a2c31f4df061f1b56d4726f6415
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-06-06 10:09:47 +1200

    Update break infinity.js

--- src/core/secret-formula/glitch/break infinity.js
+++ src/core/secret-formula/glitch/break infinity.js
@@ -3,7 +3,7 @@ export const breakinfinityUG = [
   {
     name: "limited space",
     id: 0,
-    requirement: () => `reach ${format("1e500")} antimater up to 4 dimboosts, one galaxy`,
+    requirement: () => `reach ${format("1e500")} antimater up to 4 dimboosts, one galaxy (pre-eternity)`,
     hasFailed: () => player.dimensionBoosts > 4 || player.galaxies > 1,
     checkRequirement: () => player.antimatter.e >= 500 && player.dimensionBoosts <= 4 && player.galaxies <= 1,
     checkevent: GAME_EVENT.GAME_TICK_BEFORE,
@@ -14,7 +14,7 @@ export const breakinfinityUG = [
   {
   name: "infinitly limiting",
     id: 1,
-    requirement: () => `reach ${format(2000)} (pending) infinity points with up to four dimboost and one antimatter galaxy`,
+    requirement: () => `reach ${format(2000)} (pending) infinity points with up to four dimboost and one antimatter galaxy (pre-eternity)`,
     hasFailed: () => player.dimensionBoosts > 4 || player.galaxies > 1 ,
     checkRequirement: () =>  gainedInfinityPoints().greaterThan(2000) && player.dimensionBoosts <= 4 && player.galaxies <= 1,
     checkevent: GAME_EVENT.GAME_TICK_BEFORE,
@@ -48,7 +48,7 @@ export const breakinfinityUG = [
   {
     name: "replicated infinity",
     id: 4,
-    requirement: () => `have ${format(10)} replicanti without any upgrades`,
+    requirement: () => `have ${format(10)} replicanti without any upgrades (pre-eternity)`,
     hasFailed: () => player.replicanti.chance > 0.01 || player.replicanti.interval < 1000 || PlayerProgress.eternityUnlocked(),
     checkRequirement: () => player.replicanti.amount.greaterThanOrEqualTo(10) && player.replicanti.chance == 0.01 && player.replicanti.interval >= 1000 && !PlayerProgress.eternityUnlocked(),
     checkevent: GAME_EVENT.GAME_TICK_BEFORE,
@@ -60,7 +60,7 @@ export const breakinfinityUG = [
   {
     name: "cloned replication",
     id: 5,
-    requirement: () => `have ${format(1.79e308)} replicanti without any upgrades`,
+    requirement: () => `have ${format(1.79e308)} replicanti without any upgrades (pre-eternity)`,
     hasFailed: () => player.replicanti.chance > 0.01 || player.replicanti.interval < 1000 || PlayerProgress.eternityUnlocked(),
     checkRequirement: () => player.replicanti.amount.greaterThanOrEqualTo(1.79e308) && player.replicanti.chance == 0.01 && player.replicanti.interval >= 1000 && !PlayerProgress.eternityUnlocked(),
     checkevent: GAME_EVENT.GAME_TICK_BEFORE,
