86bf828022903a4ada0e38a8e3aa24e8d41146b6
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-06-07 10:00:19 +1200

    Update infinity-dimension.js

--- src/core/dimensions/infinity-dimension.js
+++ src/core/dimensions/infinity-dimension.js
@@ -182,7 +182,7 @@ class InfinityDimensionState extends DimensionState {
       mult = mult.pow(0.5);
     }
 
-    if(mult.gte("1e1E21") && tier == 8) mult = mult.div(mult.div("1e1E21").pow(0.99));
+    if(mult.gte("1e1E21")) mult = mult.div(mult.div("1e1E21").pow(0.99));
 
     return mult;
   }
