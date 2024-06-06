4d1bb8260cbb430a7f774e400b1021d51225f7d0
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-06-06 16:22:12 +1200

    Update dark-matter-dimension-ascension-autobuyer.js

--- src/core/autobuyers/dark-matter-dimension-ascension-autobuyer.js
+++ src/core/autobuyers/dark-matter-dimension-ascension-autobuyer.js
@@ -24,7 +24,7 @@ export class DarkMatterDimensionAscensionAutobuyerState extends IntervaledAutobu
   tick() {
     super.tick();
     for (let i = 1; i <= SingularityMilestone.ascensionAutobuyers.effectValue; i++) {
-      DarkMatterDimension(i).ascend();
+      if(DarkMatterDimension(i).ascensions < 10000) DarkMatterDimension(i).ascend();
     }
   }
 }
