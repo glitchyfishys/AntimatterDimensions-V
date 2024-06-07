6e54c15e88da051a9ca61dfce7ff3205614a72d1
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-06-07 15:56:51 +1200

    Update serializer.js

--- src/core/storage/serializer.js
+++ src/core/storage/serializer.js
@@ -20,10 +20,8 @@ Decimal.prototype.toString = function () {
   if (this.e < 21 && this.e > -7) {
     return this.toNumber().toString();
   }
-  
-  const e = this.e.toString().replace("e+", "E+");
 
-  return this.m + "e" + (this.e >= 0 ? "+" : "") + e;
+  return this.m + "e" + (this.e >= 0 ? "+" : "") + this.e.toString().replace("e","E");
 };
 
 
