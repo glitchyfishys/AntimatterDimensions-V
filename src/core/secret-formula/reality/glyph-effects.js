96bc07b29707eb5c2d69a7189aad33ba56c69c42
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-06-07 13:33:44 +1200

    Update glyph-effects.js

--- src/core/secret-formula/reality/glyph-effects.js
+++ src/core/secret-formula/reality/glyph-effects.js
@@ -125,8 +125,8 @@ export const glyphEffects = {
     singleDesc: "Tachyon Galaxy threshold multiplier ×{value}",
     genericDesc: "Tachyon Galaxy cost multiplier",
     shortDesc: "TG threshold ×{value}",
-    effect: (level, strength) => 1 / (Math.pow(3000000, 0.17) * Math.pow(3.5, 0.35) * (100 +
-      GlyphAlteration.sacrificeBoost("dilation") / 50) / 10),
+    effect: (level, strength) => 1 / (Math.pow(level, 0.17) * Math.pow(strength, 0.35) * (100 +
+      GlyphAlteration.sacrificeBoost("dilation") / 50) / 100),
     formatEffect: x => format(x, 3, 3),
     alteredColor: () => GlyphAlteration.getBoostColor("dilation"),
     alterationType: ALTERATION_TYPE.BOOST,
