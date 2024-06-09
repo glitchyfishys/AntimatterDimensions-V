3df577df20f14c74d65374c7f918ae428669dcf9
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-06-10 09:25:58 +1200

    Update glyph-core.js

--- src/core/glyphs/glyph-core.js
+++ src/core/glyphs/glyph-core.js
@@ -756,7 +756,7 @@ export const Glyphs = {
   swapIntoActive(glyph, targetSlot) {
 
     if(Glyphs.active[targetSlot].type == "cursed" && Glyphs.active.filter(x => x == null ? false : x.type == "cursed" ).length <= Glitch.forceglyphs && Glitch.isRunning) {
-        Modal.message.show(`you can swap due to being forced have at least ${Glitch.forceglyphs} cursed glyphs`, { closeEvent: GAME_EVENT.GLYPHS_CHANGED });
+        Modal.message.show(`you can not swap due to being forced have at least ${Glitch.forceglyphs} cursed glyphs`, { closeEvent: GAME_EVENT.GLYPHS_CHANGED });
       return
     }
     this.removeFromInventory(glyph);
