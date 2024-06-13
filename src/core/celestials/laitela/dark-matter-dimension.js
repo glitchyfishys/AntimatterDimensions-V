ff88d88ed124a65aef02ab13a9485666827f016f
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-06-14 10:50:28 +1200

    Update dark-matter-dimension.js

--- src/core/celestials/laitela/dark-matter-dimension.js
+++ src/core/celestials/laitela/dark-matter-dimension.js
@@ -89,7 +89,7 @@ export class DarkMatterDimensionState extends DimensionState {
   }
 
   get powerDE() {
-    if (!this.isUnlocked || Pelle.isDoomed) return 0;
+    if (!this.isUnlocked || Pelle.isDoomed) return new Decimal(1);
     const tierFactor = Math.pow(15, this.tier - 1);
     const destabilizeBoost = Laitela.isFullyDestabilized ? 8 : 1;
     let DEmult = new Decimal(1 + this.data.powerDEUpgrades * 0.1)
