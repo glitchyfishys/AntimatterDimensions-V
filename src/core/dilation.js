d38e3980a76bd8e49f8206fc322b2aeec998ece2
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-06-14 09:08:45 +1200

    Update dilation.js

--- src/core/dilation.js
+++ src/core/dilation.js
@@ -234,9 +234,9 @@ export function getDilationTimeEstimate(goal) {
 export function dilatedValueOf(value) {
   let ex = Math.max(Math.log10(Math.log10(player.celestials.pelle.galaxyGenerator.generatedGalaxies + 1))/4, 0);
   const log10 = value.log10();
-  const dilationPenalty = (0.75 + ex + GlitchRifts.delta.milestones[2].effectOrDefault(0)) * Effects.product(DilationUpgrade.dilationPenalty);
+  const dilationPenalty = Math.min((0.75 + ex + GlitchRifts.delta.milestones[2].effectOrDefault(0)) * Effects.product(DilationUpgrade.dilationPenalty), 1);
   let effect = Decimal.pow10(Math.sign(log10) * Math.pow(Math.abs(log10), dilationPenalty));
-  if ( value.gte("1e1E308")) effect = new Decimal("1e1E308");
+  if ( value.gte("1e1E308")) effect = new Decimal("1e1E308"); // (Math.sign(log10) * Math.pow(Math.abs(log10), dilationPenalty)) == Infinity
   return effect
 }
 
