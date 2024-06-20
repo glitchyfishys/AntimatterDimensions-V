95c5f611abef3ef459765bc9feeb23fadcb94640
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-06-20 14:30:29 +1200

    Update tickspeed.js

--- src/core/tickspeed.js
+++ src/core/tickspeed.js
@@ -228,11 +228,11 @@ export const FreeTickspeed = {
   },
 
   fromShards(shards) {
-    const tickmult = Math.max(1 + (Effects.min(1.33, TimeStudy(171), eternityUGs.all[1] ) - 1 - GlitchRifts.beta.milestones[2].effectOrDefault(0)) *
-      Math.max(getAdjustedGlyphEffect("cursedtickspeed"), 1),1e300);
+    const tickmult = (1 + (Effects.min(1.33, TimeStudy(171), eternityUGs.all[1] ) - 1 - GlitchRifts.beta.milestones[2].effectOrDefault(0)) *
+      Math.max(getAdjustedGlyphEffect("cursedtickspeed"), 1));
     const logTickmult = Math.log(tickmult);
     const logShards = shards.ln();
-    const uncapped = Math.max(0, logShards / logTickmult);
+    const uncapped = Math.clamp(logShards / logTickmult, 0, 1e300);
     if (uncapped <= FreeTickspeed.softcap) {
       this.multToNext = tickmult;
       return {
