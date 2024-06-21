73e959409ede388e8108fd5e765134a5df16f2f7
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-06-21 13:54:58 +1200

    Update NormalAchievementsTab.vue

--- src/components/tabs/normal-achievements/NormalAchievementsTab.vue
+++ src/components/tabs/normal-achievements/NormalAchievementsTab.vue
@@ -14,8 +14,8 @@ export default {
     return {
       achievementPower: 0,
       achTPEffect: 0,
-      achCountdown: 0,
-      totalCountdown: 0,
+      achCountdown: new Decimal(),
+      totalCountdown: new Decimal(),
       missingAchievements: 0,
       showAutoAchieve: false,
       isAutoAchieveActive: false,
@@ -73,9 +73,9 @@ export default {
       const gameSpeedupFactor = getGameSpeedupFactor();
       this.achievementPower = Achievements.power;
       this.achTPEffect = RealityUpgrade(8).config.effect();
-      this.achCountdown = Achievements.timeToNextAutoAchieve / gameSpeedupFactor;
-      this.totalCountdown = ((Achievements.preReality.countWhere(a => !a.isUnlocked) - 1) * Achievements.period +
-        Achievements.timeToNextAutoAchieve) / gameSpeedupFactor;
+      this.achCountdown = Decimal.div((Achievements.timeToNextAutoAchieve), gameSpeedupFactor);
+      this.totalCountdown = Decimal.div(((Achievements.preReality.countWhere(a => !a.isUnlocked) - 1) * Achievements.period +
+        Achievements.timeToNextAutoAchieve), gameSpeedupFactor);
       this.missingAchievements = Achievements.preReality.countWhere(a => !a.isUnlocked);
       this.showAutoAchieve = PlayerProgress.realityUnlocked() && !Perk.achievementGroup5.isBought;
       this.isAutoAchieveActive = player.reality.autoAchieve;
@@ -164,17 +164,17 @@ export default {
       v-if="showAutoAchieve"
       class="c-achievements-tab__header"
     >
-      <div v-if="achCountdown > 0">
+      <div v-if="achCountdown.gt(0)">
         Automatically gain the next missing Achievement in
-        {{ timeDisplayNoDecimals(achCountdown) }}<span v-if="!isAutoAchieveActive"> once Auto is turned on</span>.
+        {{ timeDisplayNoDecimals(achCountdown.toNumber()) }}<span v-if="!isAutoAchieveActive"> once Auto is turned on</span>.
         (left-to-right, top-to-bottom)
       </div>
       <div v-else-if="missingAchievements !== 0">
         Automatically gain the next missing Achievement as soon as you enable Auto Achievements.
         (left-to-right, top-to-bottom)
       </div>
-      <div v-if="totalCountdown > 0">
-        You will regain all remaining achievements after {{ timeDisplayNoDecimals(totalCountdown) }} if Auto
+      <div v-if="totalCountdown.lt(0)">
+        You will regain all remaining achievements after {{ timeDisplayNoDecimals(totalCountdown.toNumber()) }} if Auto
         Achievement <span v-if="isAutoAchieveActive">stays enabled</span><span v-else>is turned on</span>.
       </div>
       <br>
