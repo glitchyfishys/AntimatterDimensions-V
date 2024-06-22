a30e072c1e93395cdbe66c0699b5f28365949172
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-06-22 15:27:55 +1200

    Update NormalAchievementsTab.vue

--- src/components/tabs/normal-achievements/NormalAchievementsTab.vue
+++ src/components/tabs/normal-achievements/NormalAchievementsTab.vue
@@ -73,9 +73,9 @@ export default {
       const gameSpeedupFactor = getGameSpeedupFactor();
       this.achievementPower = Achievements.power;
       this.achTPEffect = RealityUpgrade(8).config.effect();
-      this.achCountdown = Decimal.div((Achievements.timeToNextAutoAchieve), gameSpeedupFactor);
-      this.totalCountdown = Decimal.div(((Achievements.preReality.countWhere(a => !a.isUnlocked) - 1) * Achievements.period +
-        Achievements.timeToNextAutoAchieve), gameSpeedupFactor);
+      this.achCountdown = Decimal.div(Achievements.timeToNextAutoAchieve, gameSpeedupFactor).toNumber();
+      this.totalCountdown = Decimal.div(((Achievements.preReality.countWhere(a => !a.isUnlocked) - 1) * Achievements.period.toNumber() + Achievements.timeToNextAutoAchieve), gameSpeedupFactor);
+      
       this.missingAchievements = Achievements.preReality.countWhere(a => !a.isUnlocked);
       this.showAutoAchieve = PlayerProgress.realityUnlocked() && !Perk.achievementGroup5.isBought;
       this.isAutoAchieveActive = player.reality.autoAchieve;
