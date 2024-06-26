fac3bb737be0cc9df652feaf6e65d6f6731cf575
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-06-26 15:24:51 +1200

    Update BlackHoleChargingSliders.vue

--- src/components/tabs/black-hole/BlackHoleChargingSliders.vue
+++ src/components/tabs/black-hole/BlackHoleChargingSliders.vue
@@ -36,7 +36,7 @@ export default {
       this.isInverted = BlackHoles.areNegative;
       this.isLaitela = Laitela.isRunning;
       this.negativeSlider = -Decimal.log10(player.blackHoleNegative);
-      this.negativeBHDivisor = Decimal.pow10(his.negativeSlider);
+      this.negativeBHDivisor = Decimal.pow10(this.negativeSlider);
       const maxInversion = player.requirementChecks.reality.slowestBH.lte(1e-300);
       this.isDisabled = ImaginaryUpgrade(24).isLockingMechanics && Ra.isRunning && maxInversion;
     },
