a8aee9262936beb9c2a8cf408af9bc66f7fc5893
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-07-01 18:40:46 +1200

    Update glitch.js

--- src/core/secret-formula/celestials/quotes/glitch.js
+++ src/core/secret-formula/celestials/quotes/glitch.js
@@ -1,3 +1,4 @@
+
 export const glitchQuotes = {
   start: {
     id: 0,
@@ -19,8 +20,51 @@ export const glitchQuotes = {
     id: 2,
     lines: [
       "no more than eight",
-      "it looks like your the one"
+      "it looks like your the one",
+      "it took so long to find you"
+    ],
+  },
+  galaxy: {
+    id: 3,
+    lines: [
+      "ah yes galaxies, thier a bit strange",
+      "let's just say that you dimensions are not effected by them exactly",
+      "they boost tick speed (i made that for you), the effect of mass",
+      "getting more is harder due to how big they are"
+    ],
+  },
+    infinity: {
+    id: 4,
+    lines: [
+      "so fast",
+      "it is going to be fun, you and me are going to stop HIM",
+      "... i'll come back when you have infinited fast enough"
+    ],
+  },
+  break: {
+    id: 5,
+    lines: [
+      "good you found it",
+      "i hope that didn't take long",
+      "the next thing you need is replacanti",
+      "it's something i made to fill up space though it has it's limits"
+    ],
+  },
+  eternity: {
+    id: 6,
+    lines: [
+      "you may have completed challenges getting here",
+      "don't worry thiers more for you, have fun completing them all",
+      "all so the time studys i have designed them for you",
+      "i will let you make some later"
+    ],
+  },
+  dilation: {
+    id: 7,
+    lines: [
+      "i want to see your power",
+      "i want you to grow",
+      "you will need it to beat HIM"
     ],
   }
-  
 };
