40f0e634ae7ad1baf922538eb2ac79490b73e508
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-07-17 14:26:43 +1200

    Update game.js

--- src/game.js
+++ src/game.js
@@ -396,7 +396,7 @@ export function getGameSpeedupFactor(effectsToConsider, blackHolesActiveOverride
 
   // 1e-300 is now possible with max inverted BH, going below it would be possible with
   // an effarig glyph.
-  factor = Decimal.clamp(factor, 1e-300, 1e300);
+  if(player.records.fullGameCompletions > 0) factor = Decimal.clamp(factor, 1e-300, "1e450");
 
   return factor;
 }
