de99daa88f59b96f096b7a389e0107840c999c46
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-07-19 17:31:38 +1200

    Update game.js

--- src/game.js
+++ src/game.js
@@ -556,8 +556,8 @@ export function gameLoop(passDiff, options = {}) {
         GAME_SPEED_EFFECT.BLACK_HOLE, GAME_SPEED_EFFECT.SINGULARITY_MILESTONE]);
       const amplification = Ra.unlocks.improvedStoredTime.effects.gameTimeAmplification.effectOrDefault(1);
       const beforeStore = player.celestials.enslaved.stored;
-      player.celestials.enslaved.stored = Decimal.clampMax(player.celestials.enslaved.stored.add(
-        (totalTimeFactor.sub(reducedTimeFactor)).mul(amplification).mul(diff)), 1e300);
+      player.celestials.enslaved.stored = player.celestials.enslaved.stored.add(
+        (totalTimeFactor.sub(reducedTimeFactor)).mul(amplification).mul(diff));
       Enslaved.currentBlackHoleStoreAmountPerMs = player.celestials.enslaved.stored.sub(beforeStore).div(diff);
       speedFactor = reducedTimeFactor;
     }
