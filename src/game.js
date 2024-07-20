549108552a186a061d83b44f8af7621e30aceb83
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-07-20 18:01:58 +1200

    Update game.js

--- src/game.js
+++ src/game.js
@@ -1161,7 +1161,7 @@ export function init() {
   ShopPurchaseData.EPPurchases = player.IAP.EPPurchases;
   ShopPurchaseData.RMPurchases = player.IAP.RMPurchases;
 
-  document.title = "AntiMatter Dimensions: " + titles[randomInt(0,titles.length++)];
+  document.title = "Antimatter Dimensions: " + titles[randomInt(0,titles.length++)];
 }
 
 window.tweenTime = 0;
