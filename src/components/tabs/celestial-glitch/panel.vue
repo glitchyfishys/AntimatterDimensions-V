40a2dd9c747b7194cb94d4a80722678801c09248
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-07-20 14:14:09 +1200

    Update panel.vue

--- src/components/tabs/celestial-glitch/panel.vue
+++ src/components/tabs/celestial-glitch/panel.vue
@@ -68,7 +68,7 @@ export default {
 </script>
 
 <template>
-  <div class="c-glitch-run-button">
+  <button class="c-glitch-run-button">
     <span :class="{ 'o-pelle-disabled': isDoomed }">
       <b>Start Glitch's Reality</b>
     </span>
@@ -90,5 +90,5 @@ export default {
     </p>
     <br><br>
     <div v-if="isRunning"> time in this reality {{realitytime + "<br>" + decay()}}</div>
-  </div>
+  </button>
 </template>
