e467118dd384810cbb052c5ccc7ca4a15673f168
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-08-02 11:01:56 +1200

    Update RealityButton.vue

--- src/components/ui-modes/prestige-header/RealityButton.vue
+++ src/components/ui-modes/prestige-header/RealityButton.vue
@@ -107,7 +107,7 @@ export default {
       const teresaReward = this.formatScalingMultiplierText(
         "Glyph Sacrifice",
         Teresa.runRewardMultiplier,
-        Math.max(Teresa.runRewardMultiplier, Teresa.rewardMultiplier(Currency.antimatter.value)));
+        Decimal.max(Teresa.runRewardMultiplier, Teresa.rewardMultiplier(Currency.antimatter.value)));
       const teresaThreshold = this.formatThresholdText(
         Teresa.rewardMultiplier(Currency.antimatter.value).gt(Teresa.runRewardMultiplier),
         player.celestials.teresa.bestRunAM,
